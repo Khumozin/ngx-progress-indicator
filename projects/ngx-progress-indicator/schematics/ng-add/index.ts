61e9f5c11231b8cadda4fe2cd88e95564cb9712b
khumozin <Khumomogorosi@gmail.com>
2022-06-27 20:44:39 +0200

    implement library

---
+++ projects/ngx-progress-indicator/schematics/ng-add/index.ts
@@ -0,0 +1,48 @@
+import { Rule, SchematicContext, SchematicsException, Tree } from '@angular-devkit/schematics';
+import { NodePackageInstallTask } from '@angular-devkit/schematics/tasks';
+import { addImportToModule } from '@schematics/angular/utility/ast-utils';
+import { applyToUpdateRecorder } from '@schematics/angular/utility/change';
+import * as ts from 'typescript';
+
+export function ngAdd(): Rule {
+  return (tree: Tree, context: SchematicContext) => {
+    context.logger.info('Adding library module...');
+    const modulePath = '/src/app/app.module.ts';
+
+    if (!tree.exists(modulePath)) {
+      throw new SchematicsException(`${modulePath} does not exist...`);
+    }
+
+    const recorder = tree.beginUpdate(modulePath);
+
+    const content = tree.read(modulePath);
+
+    if (content === null) {
+      throw new SchematicsException(`${modulePath} does not exist...`);
+    }
+
+    const sourceFile = ts.createSourceFile(
+      modulePath,
+      content.toString(),
+      ts.ScriptTarget.Latest,
+      true
+    );
+
+    applyToUpdateRecorder(
+      recorder,
+      addImportToModule(
+        sourceFile,
+        modulePath,
+        `NgxProgressIndicatorModule.forRoot({ color: '#0083ff' })`,
+        'ngx-progress-indicator'
+      )
+    );
+
+    tree.commitUpdate(recorder);
+
+    context.logger.info('Installing dependencies...');
+    context.addTask(new NodePackageInstallTask());
+
+    return tree;
+  };
+}
