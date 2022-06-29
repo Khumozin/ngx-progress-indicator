b95ecd7387181db23328e22d0619fa481de9224e
khumozin <Khumomogorosi@gmail.com>
2022-06-29 10:38:33 +0200

    fix(ngx-progress-indicator-config.model.ts): update comments
    update comments

--- projects/ngx-progress-indicator/src/lib/ngx-progress-indicator-config.model.ts
+++ projects/ngx-progress-indicator/src/lib/ngx-progress-indicator-config.model.ts
@@ -1,17 +1,17 @@
 export class NgxProgressIndicatorConfig {
   /**
-   * The size (height) of the progress bar.
+   * The size (height) of the progress indicator.
    * Numeric values get converted to px. */
   size!: number | string;
   /**
-   * Color of the progress bar. Also used for the glow
-   * around the bar. */
+   * Color of the progress indicator. Also used for the glow
+   * around the indicator. */
   color!: string;
   /**
-   * Class name used for the progress bar element. */
+   * Class name used for the progress indicator element. */
   className!: string;
   /**
-   * How many milliseconds to wait before the progress bar
+   * How many milliseconds to wait before the progress indicator
    * animation starts after calling .start(). */
   delay!: number;
 }
